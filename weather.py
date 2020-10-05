16594698fc8ceee031da865f00dbd2bd90e55dbe
hgorr <hgorr@ah-hgorr.dhcp.mathworks.com>
2020-10-05 19:08:18 -0400

    Update for latest MATLAB release, return lists for py forecast (for demo purposes)

--- weather.py
+++ weather.py
@@ -1,134 +1,128 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Thu Jan  4 20:41:45 2018
 Modified Jan 29 2019
 
 @author: hgorr
 
-"""
+'''
 
 # weather.py
+import csv
+import datetime
+import json  
+import urllib.request
+
+BASE_URL = 'https://api.openweathermap.org/data/2.5/{}?q={},{}&units={}&appid={}'
+FORECAST_KEYS = {'current_time':'DateLocal', 'temp':'T', 'deg':'WindDir',
+                 'speed':'WindSpd', 'humidity':'RH', 'pressure':'P'}
+
 def read_backup(city):
-    import csv
-    lines = []
-    with open('backupdata.csv',newline='') as csvfile:
-        reader = csv.DictReader(csvfile,delimiter=',')
-        for row in reader:
-            lines.append(row)
-        data = ""
-        for s in lines:
-            if (s["city"]==city):
+    '''Read example data from a backup file'''
+
+    with open('backupdata.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for s in [*reader]:
+            if s['city'] == city:
                 data = dict(s)
+                data['City'] = city # Used for error checking below
                 # Convert data types
-                data['temp']=float(data['temp'])
-                data['pressure']=int(data['pressure'])
-                data['humidity']=int(data['humidity'])
-                data['temp_min']=float(data['temp_min'])
-                data['temp_max']=float(data['temp_max'])
-                data['speed']=float(data['speed'])
-                data['deg']=int(data['deg'])
-                data['lat']=float(data['lat'])
-                data['lon']=float(data['lon'])
-    return data
+                for k in ('temp', 'pressure', 'humidity', 'temp_min', 
+                          'temp_max', 'speed', 'deg', 'lat', 'lon'):
+                    data[k] = float(data[k])
+                return data
+    return None
 
 
-def get_current_weather(city, country, apikey):  
-    # get current conditions in specified location
-    # get_current_weather('boston','us',key)
-    import urllib.request
-    import json    
-    # read current conditions
-#        url = "https://api.openweathermap.org/data/2.5/weather?q=Boston,us&appid=11111"
+def get_current_weather(city, country, apikey,**kwargs):  
+    '''get current conditions in specified location
+    get_current_weather('boston','us',key,units='metric')'''
+            
+    info = {'units':'imperial'}     
+    for key, value in kwargs.items():
+        info[key] = value
+    # Read current conditions
     try:
-        url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units=imperial&appid="+apikey        
-        response = urllib.request.urlopen(url)
-        html = response.read()
-        json_data = json.loads(html)
-        
+        # url = 'https://api.openweathermap.org/data/2.5/weather?q=Boston,us&appid=11111'
+        url = BASE_URL.format('weather',city,country,info['units'],apikey)
+        json_data = json.loads(urllib.request.urlopen(url).read())        
     except urllib.error.URLError:
-        # if weather API doesnt work, read the file
-        json_data = read_backup(city)
-          
+        # If weather API doesnt work, read the file
+        json_data = read_backup(city)          
+
     return json_data
 
-def parse_json(json_data):
-    # parse and extract json data 
-    import datetime
+
+def parse_current_json(json_data):
+    '''parse and extract json data from the current weather data''' 
+
     try:
-        #select meteorological and location data from dictionary
+        # select data of interest from dictionary
         weather_info = json_data['main']
         weather_info.update(json_data['wind'])
+        weather_info.update(json_data['coord'])
         weather_info['city'] = json_data['name']
-        weather_info['lat'] = json_data['coord']['lat']
-        weather_info['lon'] = json_data['coord']['lon']
-#       # add date and time
-        now = datetime.datetime.now()
-        weather_info['current_time']=str(now)
-            
-    except KeyError:
-        # use current dictionary 
+        # add current date and time
+        weather_info['current_time'] = str(datetime.datetime.now())
+
+    except KeyError as e:
+        # use current dictionary (because it probably came from backup file) 
         try:
+            # If this fails then the json_data didn't come from backup file
             json_data.pop('City')
             weather_info = json_data
         except:
-            print('Something else went wrong')
+            # print('Something else went wrong while parsing current json')
+            raise e
+    
     return weather_info
 
-def get_forecast(city, country, apikey):
-    # get forecast conditions in specified location
-    import urllib.request
-    import json   
+
+def get_forecast(city, country, apikey, **kwargs):
+    '''get forecast conditions in specified location'''
+        
+    # include keyword args for numdays=3  and units='metric'    
+    info = {'units':'imperial', 'days':5}     
+    for key, value in kwargs.items():
+        info[key] = value
+        
+    # get forecast    
     try:
-        url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+","+country+"&units=imperial&appid="+apikey        
-        response = urllib.request.urlopen(url)
-        html = response.read()
-        json_data = json.loads(html)
+        url = BASE_URL.format('forecast',city,country,info['units'],apikey)
+        json_data = json.loads(urllib.request.urlopen(url).read())    
     except: 
-        import csv
-        lines = []
-        with open('backupforecast.csv',newline='') as csvfile:
-            reader = csv.DictReader(csvfile,delimiter=',')
-            for row in reader:
-                lines.append(row)
-        json_data = ""
-        for s in lines:
-            if (s["City"]==city):
-                json_data = dict(s)        
+        with open('backupforecast.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            json_data = {'City':city}
+            for key in FORECAST_KEYS.keys():
+                json_data[key] = []
+            for s in [*reader]:
+                if s['City'] == city:
+                    for key,value in FORECAST_KEYS.items():
+                        json_data[key].append(dict(s)[value])
     return json_data
 
-def parse_forecast(json_data):
-    import array
-    # parse forecast json data
+def parse_forecast_json(json_data):
+    '''parse and extract json data from the weather forecast data'''     
+    
     try:
+        # parse forecast json data
         data = json_data['list']
-        # create arrays
-        temp = array.array('f')
-        pressure = array.array('f')
-        humidity = array.array('f')
-        speed = array.array('f')
-        deg = array.array('f')
-        date = [];
-        
-        # loop over all and add to arrays
-        for i in range(40):
-            x1 = data[i]
-            temp.append(x1['main']['temp'])
-            pressure.append(x1['main']['pressure'])
-            humidity.append(x1['main']['humidity'])
-            speed.append(x1['wind']['speed'])
-            deg.append(x1['wind']['deg'])
-            date.append(x1['dt_txt'])
-                       
-        # create dictionary
-        weather_info = dict(current_time=date,temp=temp,deg=deg,
-                                speed=speed,humidity=humidity,pressure=pressure)         
-    except KeyError:
-        # use current dictionary 
+        wind_keys = ['deg','speed']
+        weather_info = dict(zip(FORECAST_KEYS.keys(), 
+                                [[] for i in range(len(FORECAST_KEYS))]))
+        for data_point in data[0:40]:
+            for k in list(FORECAST_KEYS.keys())[1:]: #Taking a slice so we don't add the city every time
+                weather_info[k].append(float(data_point['wind' if k in wind_keys else 'main'][k]))
+            weather_info['current_time'].append(data_point['dt_txt'])
+    except KeyError as e:
+        # use current dictionary (because it probably came from backup file) 
         try:
-            json_data.pop('City')
+            # If this fails then the json_data didn't come from backup file
+            json_data.pop('City') 
             weather_info = json_data
         except:
-            print('Something else went wrong')
+            # print('Something else went wrong while parsing forecast json')
+            raise e
+
     return weather_info
-
-
